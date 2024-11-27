a7d1c0accd745048308ab82361794ee86b22803a
kenfzhang <4808683+kenfzhang@users.noreply.github.com>
2024-11-26 17:36:49 -0800

    add text_node_to_html_node

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,7 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
+from htmlnode import LeafNode
 
 
 class TestTextNode(unittest.TestCase):
@@ -29,7 +30,59 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", TextType.NORMAL, "http://example.com")
         self.assertNotEqual(node, node2)
 
+class TestTextNodeToHTMLNode(unittest.TestCase):
+    def test_text(self):
+        tn = TextNode("This is a text node", TextType.NORMAL)
+        hn1 = text_node_to_html_node(tn)
+        hn2 = LeafNode(None, "This is a text node")
+        self.assertEqual(hn1.to_html(), hn2.to_html())
+        self.assertEqual(hn1.tag, hn2.tag)
+        self.assertEqual(hn1.value, hn2.value)
+        self.assertEqual(hn1.children, hn2.children)
+        self.assertEqual(hn1.props, hn2.props)
 
+    def test_bold_italic_code(self):
+        def fields_equal(self, hn1, hn2):
+            self.assertEqual(hn1.to_html(), hn2.to_html())
+            self.assertEqual(hn1.tag, hn2.tag)
+            self.assertEqual(hn1.value, hn2.value)
+            self.assertEqual(hn1.children, hn2.children)
+            self.assertEqual(hn1.props, hn2.props)
+
+        tn = TextNode("This is bold", TextType.BOLD)
+        hn1 = text_node_to_html_node(tn)
+        hn2 = LeafNode("b", "This is bold")
+        fields_equal(self,hn1,hn2)
+
+        tn = TextNode("This is italic", TextType.ITALIC)
+        hn1 = text_node_to_html_node(tn)
+        hn2 = LeafNode("i", "This is italic")
+        fields_equal(self,hn1,hn2)
+
+        tn = TextNode("This is code", TextType.CODE)
+        hn1 = text_node_to_html_node(tn)
+        hn2 = LeafNode("code", "This is code")
+        fields_equal(self,hn1,hn2)
+
+    def test_link(self):
+        tn = TextNode("This is a link", TextType.LINK, "https://example.com")
+        hn1 = text_node_to_html_node(tn)
+        hn2 = LeafNode("a", "This is a link", {"href": "https://example.com"})
+        self.assertEqual(hn1.to_html(), hn2.to_html())
+        self.assertEqual(hn1.tag, hn2.tag)
+        self.assertEqual(hn1.value, hn2.value)
+        self.assertEqual(hn1.children, hn2.children)
+        self.assertEqual(hn1.props, hn2.props)
+
+    def test_image(self):
+        tn = TextNode("This is an image", TextType.IMAGE, "https://example.com/image.jpg")
+        hn1 = text_node_to_html_node(tn)
+        hn2 = LeafNode("img", "", {"src": "https://example.com/image.jpg", "alt": "This is an image"})
+        self.assertEqual(hn1.to_html(), hn2.to_html())
+        self.assertEqual(hn1.tag, hn2.tag)
+        self.assertEqual(hn1.value, hn2.value)
+        self.assertEqual(hn1.children, hn2.children)
+        self.assertEqual(hn1.props, hn2.props)
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from htmlnode import LeafNode
 
 class TextType(Enum):
     NORMAL = "normal"
@@ -30,4 +31,18 @@ class TextNode:
     def __repr__(self) -> str:
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
 
-
+# Converts a TextNode to an HTMLNode
+def text_node_to_html_node(text_node):
+    if text_node.text_type == TextType.NORMAL:
+        return LeafNode(None, text_node.text)
+    if text_node.text_type == TextType.BOLD:
+        return LeafNode("b", text_node.text)
+    if text_node.text_type == TextType.ITALIC:
+        return LeafNode("i", text_node.text)
+    if text_node.text_type == TextType.CODE:
+        return LeafNode("code", text_node.text)
+    if text_node.text_type == TextType.LINK:
+        return LeafNode("a", text_node.text, {"href": text_node.url})
+    if text_node.text_type == TextType.IMAGE:
+        return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+    raise ValueError(f"Invalid text type: {text_node.text_type}")
